a7a55a37e8a7f2191248bbaf9346ab50a38ae651
youngt17 <62212957+youngt17@users.noreply.github.com>
2021-08-27 22:26:56 +0100

    Update bundle.js

--- erasedata/eraseone/bundle.js
+++ erasedata/eraseone/bundle.js
@@ -3,8 +3,12 @@ const wrapper = document.querySelector('.wrapper'),
       submitInput = form[0].querySelector('input[type="submit"]');
 
 function getDataForm(e){
+  
   e.preventDefault();
   var formData = new FormData(form[0]);
 
   alert( formData.get('eraseone') + ' - ');
 }
+document.addEventListener('DOMContentLoaded', function() {
+  submitInput.addEventListener('click', getDataForm, false);
+}, false);
