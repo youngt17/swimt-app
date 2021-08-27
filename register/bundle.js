cbb1f0d9cf8cb45b80f693b5d0d9227e933eb364
youngt17 <62212957+youngt17@users.noreply.github.com>
2021-08-27 11:10:49 +0100

    Update bundle.js

--- register/bundle.js
+++ register/bundle.js
@@ -1,44 +1,26 @@
+const form = document.querySelector('#register');
 
-ConvertFile = function () {
-  var input = document.querySelector('input').files;
-  if(!input.length){
-    alert("No file selected");
-    return;
-  } 
-  var file = input[0];
+//listen for submit event
 
-  var reader = new FileReader();
-  reader.onload = (function() {
-    return function(e) {
-      var fileData = e.target.result.trim().split('\n').map(row => row.split(','));      
-      var HEADERS = ["time", "yaw", "pitch", "roll", "heading", "ax", "ay", "az", "gx", "gy", "gz", "mx", "my", "mz"];
-         
-
-      const RESULT = {};
-
-      // Assign every heading to the result.
-      for (const HEADING of HEADERS) RESULT[HEADING] = [];
-
-      fileData.map(row =>
-          // Each row → each column, number of columns === number of headers
-          row.map((column, i) =>
-            RESULT[HEADERS[i]]
-            .push(Number(column))
-          )
-
-        );
-        console.log(RESULT);
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://127.0.0.1:8080/data/add');
-        xhr.responseType = 'json';        
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.send(JSON.stringify(RESULT));
-      };
-  })(file);
-  reader.readAsText(file);;
-};
-
-
+form.addEventListener('submit', (event) => {
+    // disable default action
+        event.preventDefault();
+  
+    // configure a request
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '127.0.0.1:8080/user/add');
+  
+  let data = new FormData(form);
+   
+    xhr.responseType = 'json';        
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+  
+    // listen for `load` event
+    xhr.onload = () => {
+    console.log(xhr.responseText);
+    }
+    });
 
 
 
