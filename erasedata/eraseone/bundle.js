89932381a0916531b7cf378df30caf26dbfcf39b
youngt17 <62212957+youngt17@users.noreply.github.com>
2021-08-27 22:21:54 +0100

    Add files via upload

---
+++ erasedata/eraseone/bundle.js
@@ -0,0 +1,10 @@
+const wrapper = document.querySelector('.wrapper'),
+      form = wrapper.querySelectorAll('.form'),
+      submitInput = form[0].querySelector('input[type="submit"]');
+
+function getDataForm(e){
+  e.preventDefault();
+  var formData = new FormData(form[0]);
+
+  alert( formData.get('eraseone') + ' - ');
+}
